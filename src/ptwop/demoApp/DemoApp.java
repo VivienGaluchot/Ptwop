b1e57a7d5b48e00749d0ff8230c8ac3950738444
VivienGaluchot <vivien.galuchot@gmail.com>
2017-04-04 22:27:57 +0200

    Demo

--- src/ptwop/demoApp/DemoApp.java
+++ src/ptwop/demoApp/DemoApp.java
@@ -32,8 +32,8 @@ import ptwop.network.tcp.TcpNServent;
 import ptwop.p2p.P2P;
 import ptwop.p2p.P2PHandler;
 import ptwop.p2p.P2PUser;
-import ptwop.p2p.flood.FloodV0;
-import ptwop.p2p.routing.DumbRouter;
+import ptwop.p2p.flood.FloodV1;
+import ptwop.p2p.routing.StockasticRouter;
 
 public class DemoApp {
 
@@ -132,7 +132,7 @@ public class DemoApp {
 			public void actionPerformed(ActionEvent arg0) {
 				try {
 					NServent manager = new TcpNServent(Integer.parseInt(listenPort.getText()));
-					p2p = new FloodV0(manager, name.getText(), new DumbRouter());
+					p2p = new FloodV1(manager, name.getText(), new StockasticRouter());
 					p2p.setMessageHandler(new Handler());
 					p2p.start();
 					listenPort.setEditable(false);
